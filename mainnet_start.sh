1b6ef609462637f3aa574724c56dd3d5916a5488
陈华毅 <chenhuayi@chenhuayideMacBook-Pro.local>
2022-03-31 15:10:12 +0800

    disable internal transaction

--- mainnet_start.sh
+++ mainnet_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-set -o allexport && source ./common-blockscout-mainnet.env && set +o allexport
+set -o allexport && source ./common-blockscout-mainnet-local.env && set +o allexport
 /usr/bin/mix do ecto.create, ecto.migrate, phx.server >>blockscout.log 2>&1
