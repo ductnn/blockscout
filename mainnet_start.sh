75e8dd50c8a9ac648115e4d628ef284b296e6730
abrahamcruise321 <abrahamcruise321@gmail.com>
2022-03-31 03:25:01 +0000

    add mainnet env

---
+++ mainnet_start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+set -o allexport && source ./common-blockscout-mainnet.env && set +o allexport
+/usr/bin/mix do ecto.create, ecto.migrate, phx.server >>blockscout.log 2>&1
--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -o allexport && source ./docker-compose/envs/common-blockscout-app.env && set +o allexport
-/usr/bin/mix do ecto.create, ecto.migrate, phx.server >>/data/mainnet/blockscout/blockscout.log 2>&1
