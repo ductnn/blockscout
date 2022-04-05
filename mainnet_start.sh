6b9e279f6af1b3080b5db49ee40a50f35223a08f
abrahamcruise321 <abrahamcruise321@gmail.com>
2022-04-05 15:05:34 +0000

    update mainnet service

--- mainnet_start.sh
+++ mainnet_start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-set -o allexport && source ./common-blockscout-mainnet-local.env && set +o allexport
-/usr/bin/mix do ecto.create, ecto.migrate, phx.server >>blockscout.log 2>&1
+set -o allexport && source ./common-blockscout-mainnet.env && set +o allexport
+#/usr/bin/mix do ecto.create, ecto.migrate, phx.server >>blockscout.log 2>&1
+/usr/bin/mix  phx.server >>blockscout.log 2>&1
