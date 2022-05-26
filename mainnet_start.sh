79f26e63ca05192939eef1414a349b11bb361a53
abrahamcruise321 <abrahamcruise321@gmail.com>
2022-05-26 11:24:54 +0800

    remove output into file

--- mainnet_start.sh
+++ mainnet_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -o allexport && source ./common-blockscout-mainnet.env && set +o allexport
-#/usr/bin/mix do ecto.create, ecto.migrate, phx.server >>blockscout.log 2>&1
-/usr/bin/mix  phx.server >>blockscout.log 2>&1
+#/usr/bin/mix do ecto.create, ecto.migrate, phx.server
+/usr/bin/mix phx.server
